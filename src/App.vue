874259dfe18955d2000e4d7d6bce57225eab428d
ireti203 <opteaoged@gmail.com>
2021-07-23 12:48:28 +0100

    new changes

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
-    <h2>Vuebnb Listings</h2>
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h2>Aloha Place</h2>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
   </div>
 </template>
 <script>
